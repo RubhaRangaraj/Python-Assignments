0c75008fa61c6b42b3801a3d55e2913de0366ea1
RubhaRangaraj <33395544+RubhaRangaraj@users.noreply.github.com>
2017-11-06 10:41:49 +0530

    Update longestString.py

--- longestString.py
+++ longestString.py
@@ -1,8 +1,5 @@
-import os
-import sys
-
 def find_longest_string(input_list):
-    print max(input_list, key=len)   		
+    print (max(input_list, key=len))   		
 			
 if __name__ == "__main__":
     input_list = ["sonam","savita","madhumitha","neha","nikhitha","shivangi","keerthi","banupriya"]
